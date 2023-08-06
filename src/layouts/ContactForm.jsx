cc4605a88f48aa9732cd16a8dc9b09e0cb23b632
Abdulwahab Al-ghanim <abdoalghanim1@gmail.com>
2023-08-06 03:03:33 +0300

    integrated EmailJS into the contact form

--- src/layouts/ContactForm.jsx
+++ src/layouts/ContactForm.jsx
@@ -1,16 +1,32 @@
 // Importing necessary dependencies and assets
-import React from "react";
+import React, {useRef} from "react";
 import "./ContactForm.css"
+import emailjs from '@emailjs/browser';
 
 // defining the contact form component
 const ContactForm = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service-id', 'template-id', form.current, 'public-key')
+          .then((result) => {
+              console.log("Email sent Succesfully");
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+    
+
     return (
         <div className="main-flex-container">
             <h2>أرسل لنا رسالة</h2>
             <h3>سيتم ايصال هذه الرسالة الى البريد الخاص بفرعنا</h3>
             <div className="horizontal-flex-container">
                 <div className="contact-form">
-                    <form action="url">
+                    <form action="url" ref={form} onSubmit={sendEmail}>
                     <div className="text-field">
                         <p>الاسم</p>
                         <input type="text" name="name" placeholder="الاسم" required />
@@ -21,9 +37,9 @@ const ContactForm = () => {
                     </div>
                     <div className="text-field">
                         <p>الرسالة</p>
-                        <input type="text" name="message" placeholder="الرسالة" required className="message-field"/>
+                        <input type="text" name="message"  className="message-field" placeholder="الرسالة" required/>
                     </div>
-                    <button type="submit" className="submit-button">ارسال</button>
+                    <button type="submit" className="submit-button" value="send">ارسال</button>
                     </form>
                 </div>
                 <div className="logo-image">
