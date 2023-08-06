ec6f9391879747c2da3c8b7650e524544e58d938
Abdulwahab Al-ghanim <abdoalghanim1@gmail.com>
2023-08-06 20:43:01 +0300

    added captcha verification to the contact form

--- src/layouts/ContactForm.jsx
+++ src/layouts/ContactForm.jsx
@@ -1,32 +1,44 @@
 // Importing necessary dependencies and assets
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import "./ContactForm.css"
 import emailjs from '@emailjs/browser';
+import ReCAPTCHA from 'react-google-recaptcha'
 
 // defining the contact form component
 const ContactForm = () => {
 
     const form = useRef();
+    const refCaptcha = useRef();
+
+    const [shown, setShown] = useState(false);
 
     const sendEmail = (e) => {
         e.preventDefault();
-    
-        emailjs.sendForm('service-id', 'template-id', form.current, 'public-key')
-          .then((result) => {
-              console.log("Email sent Succesfully");
+        const token = refCaptcha.current.getValue();
+        const params = {
+            'g-recaptcha-response': token
+        }
+
+        emailjs.send(process.env.REACT_APP_SERVICE_ID,
+                        process.env.REACT_APP_TEMPLATE_ID,
+                        params, 
+                        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+                        )
+          .then((response) => {
+              console.log("Email sent succesfully", response.status, response.text);
           }, (error) => {
               console.log(error.text);
+              
           });
       };
-    
-
+      console.log({shown})
     return (
         <div className="main-flex-container">
             <h2>أرسل لنا رسالة</h2>
             <h3>سيتم ايصال هذه الرسالة الى البريد الخاص بفرعنا</h3>
             <div className="horizontal-flex-container">
                 <div className="contact-form">
-                    <form action="url" ref={form} onSubmit={sendEmail}>
+                    <form action="url" ref={form} onSubmit={sendEmail} method="POST">
                     <div className="text-field">
                         <p>الاسم</p>
                         <input type="text" name="name" placeholder="الاسم" required />
@@ -39,7 +51,21 @@ const ContactForm = () => {
                         <p>الرسالة</p>
                         <input type="text" name="message"  className="message-field" placeholder="الرسالة" required/>
                     </div>
-                    <button type="submit" className="submit-button" value="send">ارسال</button>
+                    <ReCAPTCHA
+                      sitekey= {process.env.REACT_APP_RECAPTCHA_PUBLIC_KEY}
+                      onChange={() => setShown(true)}
+                      ref={refCaptcha}  
+                      style={{
+                        margin: "10px 0px",
+                      }} />
+                    { shown ?
+                    <button
+                        type="submit"
+                        className="submit-button" 
+                        data-sitekey= {process.env.REACT_APP_RECAPTCHA_PUBLIC_KEY}
+                        value="send"
+                        >  ارسال  </button> 
+                        : null}
                     </form>
                 </div>
                 <div className="logo-image">
